33bc1bbeb8dc069a6e6f93b2e972e394fce4d6a1
Altin Bedir <bedir.altin@hotmail.com>
2012-12-17 17:07:20 +0100

    layout aangepast

--- TeamX/TeamX/Controllers/HomeController.cs
+++ TeamX/TeamX/Controllers/HomeController.cs
@@ -11,15 +11,48 @@ namespace TeamX.Controllers
         public ActionResult Index()
         {
             ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
-
+            ViewBag.Id=0;
+            ViewBag.Datum = getMonday(ViewBag.Id);
             return View();
+        }
+
+        public ActionResult Dinsdag()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+            ViewBag.Id = 1;
+            ViewBag.Datum = getMonday(ViewBag.Id);
+            return View("Index");
+        }
+
+        public ActionResult Woensdag()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+            ViewBag.Id = 2;
+            ViewBag.Datum = getMonday(ViewBag.Id);
+            return View("Index");
+        }
+
+        public ActionResult Donderdag()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+            ViewBag.Id = 3;
+            ViewBag.Datum = getMonday(ViewBag.Id);
+            return View("Index");
+        }
+
+        public ActionResult Vrijdag()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+            ViewBag.Id = 4;
+            ViewBag.Datum = getMonday(ViewBag.Id);
+            return View("Index");
         }
 
         public ActionResult About()
         {
             ViewBag.Message = "Your app description page.";
 
-            return View();
+            return View("Index");
         }
 
         public ActionResult Contact()
@@ -27,6 +60,28 @@ namespace TeamX.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult ParserForm()
+        {
+            ViewBag.Message = "Your parser form.";
+
+            return View();
+        }
+        public ActionResult ResultPage()
+        {
+            ViewBag.Message = "Your parser result page.";
+
+            return View();
+        }
+
+        public DateTime getMonday(int id)
+        {
+            DateTime input = DateTime.Now;
+            int delta = DayOfWeek.Monday - input.DayOfWeek + id;
+            DateTime monday = input.AddDays(delta);
+
+            return monday;
         }
     }
 }
---
+++ TeamX/TeamX/Scripts/resultPage.js
@@ -0,0 +1,52 @@
+﻿var kleur = true;
+var schema = new Array();
+schema[0] = "maandag";
+schema[1] = "10:00";
+schema[2] = "12:00";
+schema[3] = "3 TI";
+schema[4] = "JAVA";
+schema[5] = "CH0"
+schema[6] = "Possemiers";
+
+document.write('<table style="background-color:#E6E6E6;width:100%;border-collapse:collapse;">')
+document.write('<tr style="background-color:orange;color:white;">')
+document.write('<th align="center" >Dag</th>')
+
+document.write('<th align="center" ">Start</th>')
+
+document.write('<th align="center" ">Einde</th>')
+
+document.write('<th align="center" ">Klas</th>')
+
+document.write('<th align="center" ">Olod</th>')
+
+document.write('<th align="center" ">Lokaal</th>')
+
+document.write('<th align="center" ">Docent</th></tr>')
+
+
+for (var i = 0; i < 5; i++) {
+    Afprinten();
+};
+
+function Afprinten() {
+    var x;
+    if (kleur == true) {
+        document.write('<tr bgcolor="#E6E6E6">')
+        kleur = false;
+
+    } else {
+        document.write('<tr bgcolor="white">')
+        kleur = true;
+    }
+
+    for (x in schema) {
+
+        document.write('<td ALIGN="center">' + schema[x] + '</td>')
+
+    }
+    document.write('</tr>')
+
+}
+
+document.write('</table>')
