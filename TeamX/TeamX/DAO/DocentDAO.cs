8dc8c7ff9b750fa93b7889ffa83499df4b981d4c
Nicolas Van Bladel <nicolaas.vanbladel@student.artesis.be>
2012-12-17 17:43:39 +0100

    DAO docenten

---
+++ TeamX/TeamX/DAO/DocentDAO.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using TeamX.Models;
+
+namespace TeamX.DAO
+{
+    public class DocentDAO
+    {
+        private static TimetableContext ctx = new TimetableContext();
+
+        public static void Add(Docent d)
+        {
+            ctx.Docents.Add(d);
+            Update();
+        }
+
+        public static void Remove(Docent d)
+        {
+            ctx.Docents.Remove(d);
+            Update();
+        }
+
+        public static void Update()
+        {
+            ctx.SaveChanges();
+        }
+
+        public Docent GetDocentById(int id = -1)
+        {
+            return ctx.Docents.Find(id);
+        }
+
+        public List<Docent> GetDocentByVoornaam(string voornaam)
+        {
+            var result = from doc in ctx.Docents
+                         where doc.voornaam == voornaam
+                         select doc;
+            var col = new List<Docent>(result);
+            return col;
+        }
+        
+    }
+}
