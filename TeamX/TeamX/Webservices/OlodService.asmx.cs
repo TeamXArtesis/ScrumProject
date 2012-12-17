2f1b35529ccefdada82913151fa250ffd340111d
Selim Kutlu <Selim.kutlu@student.artesis.be>
2012-12-17 16:32:30 +0100

    Revert "olodomschrijving testen"
    This reverts commit e0efe404e7b2dd26a6a609e710c75a681aebbd01.

--- TeamX/TeamX/Webservices/OlodService.asmx.cs
+++ TeamX/TeamX/Webservices/OlodService.asmx.cs
@@ -68,18 +68,6 @@ namespace TeamX.Webservices
             string json = JsonConvert.SerializeObject(result, Formatting.Indented, serSettings);
             return json;
         }
-        // testen
-        [WebMethod]
-        [ScriptMethod(ResponseFormat = ResponseFormat.Json)]
-        public string GetOlodByOmschrijving(string omschrijf)
-        {
-            JavaScriptSerializer js = new JavaScriptSerializer();
-            var result = from olod in ctx.Olods
-                         where olod.omschrijving.Equals(omschrijf)
-                         select olod;
-
-            string json = JsonConvert.SerializeObject(result, Formatting.Indented, serSettings);
-            return json;
-        }
+        
     }
 }
